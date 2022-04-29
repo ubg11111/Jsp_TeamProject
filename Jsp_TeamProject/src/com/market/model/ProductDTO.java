7d2c508ba09ade80165c9d376ffa9512cd81342e
bangdal <ubg11111@naver.com>
2022-04-29 16:53:53 +0900

    메인수정

---
+++ Jsp_TeamProject/src/com/market/model/ProductDTO.java
@@ -0,0 +1,103 @@
+package com.market.model;
+
+public class ProductDTO {
+
+	private int pnum;
+	private String pname;
+	private String pcategory;
+	private String pspec;
+	private String pimage;
+	private int pqty;
+	private int price;
+	private String pcompany;
+	private String pdetails;
+	private String pinputdate;
+	private String pdiscount;
+	private String pdelivery;
+	private String packages;
+	
+	
+	
+	
+	public int getPnum() {
+		return pnum;
+	}
+	public void setPnum(int pnum) {
+		this.pnum = pnum;
+	}
+	public String getPname() {
+		return pname;
+	}
+	public void setPname(String pname) {
+		this.pname = pname;
+	}
+	public String getPcategory() {
+		return pcategory;
+	}
+	public void setPcategory(String pcategory) {
+		this.pcategory = pcategory;
+	}
+	public String getPspec() {
+		return pspec;
+	}
+	public void setPspec(String pspec) {
+		this.pspec = pspec;
+	}
+	public String getPimage() {
+		return pimage;
+	}
+	public void setPimage(String pimage) {
+		this.pimage = pimage;
+	}
+	public int getPqty() {
+		return pqty;
+	}
+	public void setPqty(int pqty) {
+		this.pqty = pqty;
+	}
+	public int getPrice() {
+		return price;
+	}
+	public void setPrice(int price) {
+		this.price = price;
+	}
+	public String getPcompany() {
+		return pcompany;
+	}
+	public void setPcompany(String pcompany) {
+		this.pcompany = pcompany;
+	}
+	public String getPdetails() {
+		return pdetails;
+	}
+	public void setPdetails(String pdetails) {
+		this.pdetails = pdetails;
+	}
+	public String getPinputdate() {
+		return pinputdate;
+	}
+	public void setPinputdate(String pinputdate) {
+		this.pinputdate = pinputdate;
+	}
+	public String getPdiscount() {
+		return pdiscount;
+	}
+	public void setPdiscount(String pdiscount) {
+		this.pdiscount = pdiscount;
+	}
+	public String getPdelivery() {
+		return pdelivery;
+	}
+	public void setPdelivery(String pdelivery) {
+		this.pdelivery = pdelivery;
+	}
+	public String getPackages() {
+		return packages;
+	}
+	public void setPackages(String packages) {
+		this.packages = packages;
+	}
+	
+	
+	
+}
