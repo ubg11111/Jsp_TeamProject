7c45a1e54a7005e857cd81b35c0b7a3ff2ef5605
kimmincho <hcv21036@gmail.com>
2022-05-02 11:23:02 +0900

    1. admin action 페이지 추가
    2.product DAO,DTO 추가/
    3.SQL 변경(컬럼 삭제 및 순서변경)
    4. mapping properties 추가
    5. 깃이그노어 .classpath, .settings 추가

--- Jsp_TeamProject/WebContent/sql/product_market.sql
+++ Jsp_TeamProject/WebContent/sql/product_market.sql
@@ -1,15 +1,13 @@
-create table prduct_market(
+
+create table product_market(
    pnum number(10) primary key,				-- 상품 번호
    pname varchar2(50) not null,				-- 상품명
-   pcategory varchar2(10) not null,			-- 카테고리 코드
-   pspec varchar2(30),						-- 상품 스펙
+   pcategory_fk varchar2(10) not null,	    -- 카테고리 코드
+   pcompany varchar2(50),					-- 제조사
    pimage varchar2(100),					-- 상품 이미지
    pqty number (10) default 0,				-- 수량
    price number(10) default 0,				-- 가격
-   pcompany varchar2(20),					-- 제조사
-   pdetails varchar2(1000),							-- 설명
-   pinputdate date,							-- 입고일
-   pdiscount varchar2(5) default 0,			-- 할인율
-   pdelivery varchar2(10),					-- 배송형태
-   package varchar2(10)   					-- 포장타입
+   pspec varchar2(30),						-- 상품 스펙  
+   pdetails varchar2(1000),					-- 상품 설명
+   pinputdate date,							-- 상품 입고일
 );
